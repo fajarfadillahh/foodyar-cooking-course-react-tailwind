32e277139d2cfe714b5b8e00698d9afd7f7d1d62
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-22 11:33:09 +0700

    setup project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+      fontFamily: {
+        sans: ["Quicksand", "sans-serif"],
+      },
+      transitionDuration: {
+        400: "0.4s",
+      },
+    },
   },
   plugins: [],
-}
+};
