a6e1880e10aed68d94130371c0cd50442eff870e
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-22 21:59:30 +0700

    dark mode Testimonial

--- src/parts/HomePage/Testimonial.jsx
+++ src/parts/HomePage/Testimonial.jsx
@@ -5,9 +5,9 @@ import TestimonialImg from "../../assets/images/testimonial-image.png";
 
 export default function Testimonial() {
   return (
-    <section className="testimonial section">
+    <section className="testimonial section transition-all duration-400 dark:bg-gray-900">
       <div className="testimonial__container container grid justify-items-center gap-8">
-        <h1 className="testimonial__comment text-center text-[24px] font-medium text-gray-900 xs:max-w-sm sm:max-w-md sm:text-[28px] md:max-w-lg md:text-[32px] lg:max-w-2xl lg:text-[38px] xl:max-w-4xl">
+        <h1 className="testimonial__comment text-center text-[24px] font-medium text-gray-900 dark:text-white xs:max-w-sm sm:max-w-md sm:text-[28px] md:max-w-lg md:text-[32px] lg:max-w-2xl lg:text-[38px] xl:max-w-4xl">
           "Awalnya saya malu dengan mertua karena tidak bisa memasak, kini
           mereka lebih suka makan malam di rumah demi menyantap masakan yang
           saya buat sendiri."
@@ -21,10 +21,10 @@ export default function Testimonial() {
           />
 
           <div>
-            <h3 className="testimonial__name pb-1 text-[17px] font-semibold text-gray-900 md:text-[20px]">
+            <h3 className="testimonial__name pb-1 text-[17px] font-semibold text-gray-900 dark:text-white md:text-[20px]">
               Camella Sarah
             </h3>
-            <p className="testimonial__profession text-gray-600">
+            <p className="testimonial__profession text-gray-600 dark:text-white">
               Vege. Master
             </p>
           </div>
