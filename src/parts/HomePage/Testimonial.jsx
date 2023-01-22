4a41ad0787803b39705214ea08c464538dc20412
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-22 20:12:14 +0700

    responsive Testimonial

--- src/parts/HomePage/Testimonial.jsx
+++ src/parts/HomePage/Testimonial.jsx
@@ -7,7 +7,7 @@ export default function Testimonial() {
   return (
     <section className="testimonial section">
       <div className="testimonial__container container grid justify-items-center gap-8">
-        <h1 className="testimonial__comment text-center text-[24px] font-medium text-gray-900">
+        <h1 className="testimonial__comment text-center text-[24px] font-medium text-gray-900 xs:max-w-sm sm:max-w-md sm:text-[28px] md:max-w-lg md:text-[32px] lg:max-w-2xl lg:text-[38px] xl:max-w-4xl">
           "Awalnya saya malu dengan mertua karena tidak bisa memasak, kini
           mereka lebih suka makan malam di rumah demi menyantap masakan yang
           saya buat sendiri."
@@ -21,7 +21,7 @@ export default function Testimonial() {
           />
 
           <div>
-            <h3 className="testimonial__name pb-1 text-[17px] font-semibold text-gray-900">
+            <h3 className="testimonial__name pb-1 text-[17px] font-semibold text-gray-900 md:text-[20px]">
               Camella Sarah
             </h3>
             <p className="testimonial__profession text-gray-600">
