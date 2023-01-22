6675e0e5dec61f8a5c74eea70f43c29af62fc429
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-22 20:15:48 +0700

    responsive Statistic

--- src/parts/HomePage/Statistic.jsx
+++ src/parts/HomePage/Statistic.jsx
@@ -4,9 +4,9 @@ export default function Statistic() {
   return (
     <section className="statistic py-16">
       <div className="statistic__container container">
-        <div className="statistic__group grid grid-cols-2 justify-items-center gap-6">
+        <div className="statistic__group grid grid-cols-2 justify-items-center gap-6 md:mx-auto md:max-w-lg lg:max-w-3xl lg:grid-cols-4 xl:max-w-5xl">
           <div className="statistic__content text-center">
-            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900">
+            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900 xs:text-[32px] sm:text-[36px]">
               180.000+
             </h3>
             <p className="statistic__content-text text-[14px] text-gray-600">
@@ -15,7 +15,7 @@ export default function Statistic() {
           </div>
 
           <div className="statistic__content text-center">
-            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900">
+            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900 xs:text-[32px] sm:text-[36px]">
               20.000+
             </h3>
             <p className="statistic__content-text text-[14px] text-gray-600">
@@ -24,7 +24,7 @@ export default function Statistic() {
           </div>
 
           <div className="statistic__content text-center">
-            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900">
+            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900 xs:text-[32px] sm:text-[36px]">
               2800+
             </h3>
             <p className="statistic__content-text text-[14px] text-gray-600">
@@ -33,7 +33,7 @@ export default function Statistic() {
           </div>
 
           <div className="statistic__content text-center">
-            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900">
+            <h3 className="statistic__content-number pb-1 text-[28px] font-bold text-gray-900 xs:text-[32px] sm:text-[36px]">
               1.200.000+
             </h3>
             <p className="statistic__content-text text-[14px] text-gray-600">
